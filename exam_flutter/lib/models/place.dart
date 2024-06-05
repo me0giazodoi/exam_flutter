4c7dbd80c89eb4425240748d7d89d3aae59bc3e1
me0giazodoi <hiepga1243@gmail.com>
2024-06-05 15:30:31 +0700

    exam

---
+++ exam_flutter/lib/models/place.dart
@@ -0,0 +1,15 @@
+class Place {
+  final int id;
+  final String name;
+  final String description;
+
+  Place({required this.id, required this.name, required this.description});
+
+  factory Place.fromJson(Map<String, dynamic> json) {
+    return Place(
+      id: json['id'],
+      name: json['name'],
+      description: json['description'],
+    );
+  }
+}
